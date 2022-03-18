8025f906f41e52db0b5c365bf491f8f1d828c211
dev <>
2022-03-18 15:38:10 +0500

    home page top 2 sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { CateringComponent } from './catering/catering.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    CateringComponent
   ],
   imports: [
     BrowserModule
